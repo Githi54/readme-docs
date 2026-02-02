1555852e1def8420bab22c0142ae5a3a41a1bf82
Githi54 <chinter.games1@gmail.com>
2026-02-02 13:16:51 +0200

    refactoring

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -3,16 +3,21 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
       wrap: true,
     },
     // GFM is enabled by default in Astro
-  }
+  },
+
+  integrations: [preact()]
 });
